df32b5460eb63b76b90a6167192e145587cac7e5
asunm295222204758 <144761969+asunm295222204758@users.noreply.github.com>
2025-03-27 06:44:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -151,4 +151,5 @@ def protected():
     return jsonify({"message": f"Hello, {get_jwt_identity()}! You have access."}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.getenv('PORT', 5000))  # Read PORT from environment or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
